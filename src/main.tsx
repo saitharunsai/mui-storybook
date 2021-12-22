59ae82470109a684f358e32e20561d5f2eda33f0
ryota-murakami <dojce1048@gmail.com>
2021-12-22 18:15:04 +0900

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider as DesignTokenProvider } from '@mui/material/styles'
+import {
+  ThemeProvider as DesignTokenProvider,
+  createTheme as createDesignToken,
+} from '@mui/material/styles'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider as ReduxProvider } from 'react-redux'
 
-import designToken from '../designToken/createTheme'
-
+import { muiDefault } from './../designToken/muiDefault'
 import { store } from './app/store'
 import Routes from './Routes'
+
+const designToken = createDesignToken(muiDefault)
 
 ReactDOM.render(
   <React.StrictMode>
