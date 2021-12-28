dc6540fa201d1a1563cbc6e7986900836a5df166
ryota-murakami <dojce1048@gmail.com>
2021-12-28 11:06:53 +0900

    fix type

--- mui/components/Surfaces/AppBar/AppBar.stories.tsx
+++ mui/components/Surfaces/AppBar/AppBar.stories.tsx
@@ -1,10 +1,14 @@
+import MenuIcon from '@mui/icons-material/Menu'
 import AppBar from '@mui/material/AppBar'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 import { Box } from '@mui/system'
 import type { ComponentStory, ComponentMeta } from '@storybook/react'
+import React from 'react'
 
 import { paletteNameList } from '../../../../designToken'
-
-import DemoComponents from './Demo'
 
 export default {
   title: 'MUI Default/Surfaces/App Bar',
@@ -32,9 +36,32 @@ export const Default: ComponentStory<TODO> = (props) => (
   </Box>
 )
 
-const DemoTemplate: ComponentStory<TODO> = (props) => (
-  <DemoComponents {...props} />
-)
+const DemoTemplate: ComponentStory<TODO> = ({
+  bgcolor = 'primary.main',
+  text = 'MUI v5 Storybook Design System',
+}) => {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar sx={{ bgcolor: bgcolor }} position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {text}
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  )
+}
 export const Demo = DemoTemplate.bind({})
 Demo.args = {
   bgcolor: {
