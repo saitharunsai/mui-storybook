f80930e259fe6e9a0278f5c1d33aad577e455a1e
ryota-murakami <dojce1048@gmail.com>
2021-12-28 11:57:28 +0900

    up

--- src/Canvas.style.tsx
+++ src/Canvas.style.tsx
@@ -1,15 +1,19 @@
+import MuiContainer from '@mui/material/Container'
+import { styled } from '@mui/system'
+
 import { designToken } from '../designToken'
+
+export const Container = styled(MuiContainer)`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export const sx = {
   card: {
     width: 275,
     height: 236,
     border: `1px solid ${designToken.palette.grey[300]}`,
-  },
-  container: {
-    height: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 }
--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -2,15 +2,14 @@ import { CardContent } from '@mui/material'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
-import Container from '@mui/material/Container'
 import Text from '@mui/material/Typography'
 import React from 'react'
 
-import { sx } from './Canvas.style'
+import { sx, Container } from './Canvas.style'
 
 const Canvas: React.FC = () => {
   return (
-    <Container sx={sx.container}>
+    <Container>
       <Card sx={sx.card}>
         <CardContent sx={{ width: 275, height: 184 }}>
           <Text variant="body2" color="text.secondary" sx={{ height: 28 }}>
