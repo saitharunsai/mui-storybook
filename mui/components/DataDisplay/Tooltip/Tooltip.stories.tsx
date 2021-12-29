0cfd55790ccc42295075dea0f79e3dd0ed7e66ac
ryota-murakami <dojce1048@gmail.com>
2021-12-29 16:27:52 +0900

    fix tooltip

--- mui/components/DataDisplay/Tooltip/Tooltip.stories.tsx
+++ mui/components/DataDisplay/Tooltip/Tooltip.stories.tsx
@@ -1,26 +1,20 @@
+import DeleteIcon from '@mui/icons-material/Delete'
+import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
-
-import { paletteNameList } from '../../../../designToken'
 
 export default {
   title: 'MUI Default/DataDisplay/Tooltip',
   component: Tooltip,
-  argTypes: {
-    bgcolor: {
-      description: 'bgcolor',
-      options: paletteNameList,
-      control: {
-        type: 'radio',
-        labels: paletteNameList,
-      },
-    },
-  },
 } as ComponentMeta<TODO>
 
-const DefaultTemplate: ComponentStory<TODO> = ({
-  children = 'Hello World',
-}) => <Tooltip title={children} children={children} />
+const DefaultTemplate: ComponentStory<TODO> = () => (
+  <Tooltip title="Delete">
+    <IconButton>
+      <DeleteIcon />
+    </IconButton>
+  </Tooltip>
+)
 
 export const Default = DefaultTemplate.bind({})
 Default.args = {}
