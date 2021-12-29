d40a7486f8e0eccd341978df85709b174238299e
ryota-murakami <dojce1048@gmail.com>
2021-12-29 16:34:22 +0900

    fix accrodion

--- mui/components/Surfaces/Accordion/Accordion.stories.tsx
+++ mui/components/Surfaces/Accordion/Accordion.stories.tsx
@@ -1,27 +1,59 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import Accordion from '@mui/material/Accordion'
+import AccordionDetails from '@mui/material/AccordionDetails'
+import AccordionSummary from '@mui/material/AccordionSummary'
+import Typography from '@mui/material/Typography'
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
-
-import { paletteNameList } from '../../../../designToken'
+import * as React from 'react'
 
 export default {
   title: 'MUI Default/Surfaces/Accordion',
   component: Accordion,
-  argTypes: {
-    bgcolor: {
-      description: 'bgcolor',
-      options: paletteNameList,
-      control: {
-        type: 'radio',
-        labels: paletteNameList,
-      },
-    },
-  },
 } as ComponentMeta<TODO>
 
-const DefaultTemplate: ComponentStory<TODO> = ({
-  children = 'Hello World',
-  bgcolor = 'primary.main',
-}) => <Accordion sx={{ bgcolor: bgcolor }} children={children} />
+const DefaultTemplate: ComponentStory<TODO> = () => (
+  <div>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography>Accordion 1</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel2a-content"
+        id="panel2a-header"
+      >
+        <Typography>Accordion 2</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </Typography>
+      </AccordionDetails>
+    </Accordion>
+    <Accordion disabled>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel3a-content"
+        id="panel3a-header"
+      >
+        <Typography>Disabled Accordion</Typography>
+      </AccordionSummary>
+    </Accordion>
+  </div>
+)
 
 export const Default = DefaultTemplate.bind({})
 Default.args = {}
