0ad32b350a31bbf4ee948a3a772549bd94fc2e3f
ryota-murakami <dojce1048@gmail.com>
2021-12-29 19:37:34 +0900

    add react-docgen-typescript-plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,11 @@
+const ts = require('typescript')
+const ReactDocgenTypescriptPlugin = require('react-docgen-typescript-plugin').default
+
 module.exports = {
   'stories': [
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
     '../mui/**/*.stories.@(js|jsx|ts|tsx)',
-    '../docs/**/*.stories.mdx',
+    '../docs/**/*.stories.mdx'
   ],
   'addons': [
     '@storybook/addon-links',
@@ -11,5 +14,6 @@ module.exports = {
   'framework': '@storybook/react',
   'core': {
     'builder': 'storybook-builder-vite'
-  }
+  },
+  'plugin': [new ReactDocgenTypescriptPlugin()]
 }
