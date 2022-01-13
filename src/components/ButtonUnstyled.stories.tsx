a9cdd98ed87a7a7d7c16e2d121860eb7aa36d790
ryota-murakami <dojce1048@gmail.com>
2022-01-13 20:10:04 +0900

    ButtonUnstyled

---
+++ src/components/ButtonUnstyled.stories.tsx
@@ -0,0 +1,9 @@
+import { ButtonUnstyled } from '@mui/material'
+import React from 'react'
+
+export default {
+  title: 'ButtonUnstyled',
+  component: ButtonUnstyled,
+}
+
+export const Default = () => <ButtonUnstyled>Hello</ButtonUnstyled>
