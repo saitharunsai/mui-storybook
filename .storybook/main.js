7443277db5e7673909e16d7127db15d773b0bbde
ryota-murakami <dojce1048@gmail.com>
2022-01-26 16:14:49 +0900

    conf

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y']
+  addons: ['@storybook/addon-essentials', '@storybook/addon-storysource', '@storybook/addon-a11y', {
+    name: '@storybook/addon-docs',
+    options: {
+      sourceLoaderOptions: {
+        injectStoryParameters: false
+      }
+    }
+  }]
 }
