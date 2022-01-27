b1b7d8d2abaf7de3e61978c787984d1a10abe4af
ryota-murakami <dojce1048@gmail.com>
2022-01-27 22:42:28 +0900

    tweek

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
 
-  addons: ['@storybook/addon-storysource', {
-    name: '@storybook/addon-docs',
-    options: {
-      sourceLoaderOptions: {
-        injectStoryParameters: false
-      }
-    }
-  }, '@storybook/addon-controls', '@storybook/addon-backgrounds', '@storybook/addon-actions', '@storybook/addon-viewport', '@storybook/addon-toolbars'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5'
   }
