575c6365a0031ca34a126250c48a4e4d95af8fd9
ryota-murakami <dojce1048@gmail.com>
2022-03-05 00:16:47 +0900

    remove unused types

--- src/designToken/types/StandardCssProperties.d.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-import type * as CSS from './csstype'
-/* eslint-enable */
-
-export type StandardCSSProperties = CSS.PropertiesFallback<number | string>
--- src/designToken/types/sx.d.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-import type { CSSObject } from '@mui/styled-engine'
-/* eslint-enable */
-import type { SxProps } from './styleFunctionSx'
-
-export default function sx<T extends object = {}>(styles: SxProps<T>): CSSObject
