33ba4cb40218d382f99b51d4b8eb9d3c9ba15f3f
ryota-murakami <dojce1048@gmail.com>
2022-03-05 12:58:45 +0900

    clean

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.js', '../src/**/*.stories.tsx'],
 
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials'],
   core: {
     builder: 'webpack5'
   }
