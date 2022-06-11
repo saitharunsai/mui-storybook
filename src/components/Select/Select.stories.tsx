67931cd6f20fed0c4a89fa1eaacd03cb16d64b7c
saitharunsai <janagamsaitarun@gmail.com>
2022-06-11 18:50:27 +0530

    feat: modifed inteface for select

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,3 +1,4 @@
+import { MenuItem } from '@mui/material'
 import type { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 
@@ -9,5 +10,9 @@ export default {
 } as ComponentMeta<typeof Select>
 
 export const Default: ComponentStory<typeof Select> = (props) => (
-  <Select {...props} />
+  <Select id="select" value={10} label="Age" onChange={console.log} {...props}>
+    <MenuItem value={10}>Ten</MenuItem>
+    <MenuItem value={20}>Twenty</MenuItem>
+    <MenuItem value={30}>Thirty</MenuItem>
+  </Select>
 )
